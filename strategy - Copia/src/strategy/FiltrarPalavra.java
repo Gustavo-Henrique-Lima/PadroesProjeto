6955bf53dd3ff3f19b983d4fdd82ee092c8deb9e
Gustavo Henrique <gustavohgustavo@hotmail.com>
2023-03-04 18:22:08 -0300

    Teste GitIgnore

---
+++ strategy - Copia/src/strategy/FiltrarPalavra.java
@@ -0,0 +1,28 @@
+package strategy;
+
+public class FiltrarPalavra implements Filter
+{
+	private String palavra;
+
+	public FiltrarPalavra(String palavra) 
+	{
+		this.palavra = palavra;
+	}
+	
+	
+	public String getPalavra() 
+	{
+		return palavra;
+	}
+
+	public void setPalavra(String palavra) 
+	{
+		this.palavra = palavra;
+	}
+
+	@Override
+	public boolean interessa(String frase) 
+	{
+		return frase.toLowerCase().contains(this.palavra)?true:false;
+	}
+}
