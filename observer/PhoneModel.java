655a3aac2e5281c97d7ea7bea59465408089f142
Gustavo Henrique <gustavohgustavo@hotmail.com>
2023-03-12 12:04:52 -0300

    Fix bug

--- observer/PhoneModel.java
+++ observer/PhoneModel.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 public class PhoneModel implements Observador {
 	private List<Integer> digits = new ArrayList<>();
-	public ArrayList<Observador> observadores = new ArrayList<>();
 
 	public void addDigit(int newDigit) 
 	{
@@ -16,22 +15,6 @@ public class PhoneModel implements Observador {
 	public List<Integer> getDigits() 
 	{
 		return digits;
-	}
-
-	public void addObservador(Observador o) 
-	{
-		if (!observadores.contains(o)) 
-		{
-			observadores.add(o);
-		}
-	}
-
-	public void removerObservador(Observador o) 
-	{
-		if (observadores.contains(o)) 
-		{
-			observadores.remove(o);
-		}
 	}
 
 	@Override
