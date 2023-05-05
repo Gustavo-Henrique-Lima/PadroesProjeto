2e1552a26ad2411eb235e4b9b04757aa71770330
Gustavo Henrique <gustavohgustavo@hotmail.com>
2023-05-05 15:53:31 -0300

    Padrão Composite

---
+++ composite/src/composite/Carro.java
@@ -0,0 +1,26 @@
+package composite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Carro implements Pedaco{
+	
+	private List<Pedaco> pecas=new ArrayList<>();
+
+	public Carro()
+	{
+		pecas.add(new Chassi());
+		pecas.add(new Carroceria());
+	}
+
+	@Override
+	public double calcularPeso() {
+		double total=0;
+		for(Pedaco p:pecas)
+		{
+			total+=p.calcularPeso();
+		}
+		System.out.println("Peso final do carro: "+String.format("%.2f", total));
+		return total;
+	}
+}
---
+++ composite/src/composite/Pedaco.java
@@ -0,0 +1,5 @@
+package composite;
+
+public interface Pedaco {
+	double calcularPeso();
+}
