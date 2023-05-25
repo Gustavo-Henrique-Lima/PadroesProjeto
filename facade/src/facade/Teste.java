b415e170f3a9d0bfd7282ebfec8a3deb3ffd0ad7
Gustavo Henrique <gustavohgustavo@hotmail.com>
2023-05-25 15:02:17 -0300

    Padrão Facade

--- facade/src/facade/Teste.java
+++ facade/src/facade/Teste.java
@@ -7,6 +7,7 @@ public class Teste {
 		 ServicoEstoque estoque=new ServicoEstoque();
 		 ServicoPagamento pagamento=new ServicoPagamento();
 		 ServicoRemessa remessa=new ServicoRemessa();
+		 PedidoFacade facade=new PedidoFacade(estoque,pagamento,remessa)
 		 */
 		Produto p1=new Produto(200, "PS5");
 		PedidoFacade.registrarPedido(p1);
