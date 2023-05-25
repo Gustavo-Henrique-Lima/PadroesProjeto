4485fab58dd6e3b505f117daa8fdf4c6f4977762
Gustavo Henrique <gustavohgustavo@hotmail.com>
2023-05-25 14:44:06 -0300

    Padrão Facade

--- facade/src/facade/PedidoFacade.java
+++ facade/src/facade/PedidoFacade.java
@@ -1,7 +1,22 @@
 package facade;
 
 public class PedidoFacade {
+	/*
+	private ServicoEstoque estoque;
+	private ServicoPagamento pagamento;
+	private ServicoRemessa remessa;
 	
+	public PedidoFacade()
+	{
+	}
+	
+	public PedidoFacade(ServicoEstoque estoque, ServicoPagamento pagamento, ServicoRemessa remessa) {
+		this.estoque = estoque;
+		this.pagamento = pagamento;
+		this.remessa = remessa;
+	}
+	*/
+
 	public static void registrarPedido(Produto produto)
 	{
 		ServicoEstoque.isAvailable(produto);
--- facade/src/facade/Teste.java
+++ facade/src/facade/Teste.java
@@ -3,7 +3,11 @@ package facade;
 public class Teste {
 
 	public static void main(String[] args) {
-		
+		/*
+		 ServicoEstoque estoque=new ServicoEstoque();
+		 ServicoPagamento pagamento=new ServicoPagamento();
+		 ServicoRemessa remessa=new ServicoRemessa();
+		 */
 		Produto p1=new Produto(200, "PS5");
 		PedidoFacade.registrarPedido(p1);
 	}
